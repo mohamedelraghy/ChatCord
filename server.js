dad4623e784483b6140cac49445f3eba407920a0
mohamed El-Raghy <elraghy8@gmail.com>
2023-02-19 18:35:04 +0200

    path formatMessage to every emit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,24 @@ const io = socketio(server);
 // set Static Folder 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
+const botName = 'Chat Cord Bot';
+
 // Run when client connect
 io.on('connection', socket => {
 
     //welcome current user
-    socket.emit('message', 'Welcome to ChatCord!');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     // Run when client disconnect
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     socket.on('chatMessage', msg => {
-        io.emit('message', msg); 
+        io.emit('message', formatMessage('User', msg)); 
     });
 });
 
